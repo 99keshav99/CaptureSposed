c18047623462eedd896957b720ee313b111846ce
99keshav99 <mkin1999@gmail.com>
2024-05-15 01:39:17 -0400

    Add Dummy Toggle Switch

--- app/src/main/java/com/keshav/capturesposed/MainActivity.kt
+++ app/src/main/java/com/keshav/capturesposed/MainActivity.kt
@@ -6,21 +6,28 @@ import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.wrapContentSize
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
+import androidx.compose.material.Switch
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.mutableIntStateOf
+import androidx.compose.runtime.mutableStateOf
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import com.keshav.capturesposed.ui.theme.APPTheme
 
 class MainActivity : ComponentActivity() {
     private var counter = mutableIntStateOf(0)
+    private var isSwitchOn = mutableStateOf(false)
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
@@ -48,26 +55,35 @@ class MainActivity : ComponentActivity() {
         super.onStop()
         unregisterScreenCaptureCallback(screenCaptureCallback)
     }
-}
 
-@Composable
-fun AppUI(counter: Int, modifier: Modifier = Modifier) {
-    Column(
-        modifier = modifier
-            .fillMaxSize()
-            .wrapContentSize(Alignment.Center),
-        verticalArrangement = Arrangement.Center,
-        horizontalAlignment = Alignment.CenterHorizontally
-    ) {
-        Text(
-            text = "APP!",
-            fontSize = 160.sp,
-            textAlign = TextAlign.Center
-        )
-        Text(
-            text = "Counter: $counter",
-            fontSize = 50.sp,
-            textAlign = TextAlign.Center
-        )
+    @Composable
+    fun AppUI(counter: Int, modifier: Modifier = Modifier) {
+        Column(
+            modifier = modifier
+                .fillMaxSize()
+                .wrapContentSize(Alignment.Center),
+            verticalArrangement = Arrangement.Center,
+            horizontalAlignment = Alignment.CenterHorizontally
+        ) {
+            Row(
+                verticalAlignment = Alignment.CenterVertically
+            ) {
+                Text(
+                    text = "CaptureSposed:",
+                    fontSize = 40.sp,
+                    textAlign = TextAlign.Center
+                )
+                Switch(
+                    checked = isSwitchOn.value,
+                    onCheckedChange = { isSwitchOn.value = it }
+                )
+            }
+            Text(
+                text = "Counter: $counter",
+                fontSize = 40.sp,
+                textAlign = TextAlign.Center,
+                modifier = Modifier.padding(50.dp),
+            )
+        }
     }
 }
