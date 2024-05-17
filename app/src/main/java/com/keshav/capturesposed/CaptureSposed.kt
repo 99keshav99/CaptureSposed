e24433160fe982712d5fa937bad85d435264ffb3
David Berdik <berdikd@duq.edu>
2024-05-17 00:06:27 -0400

    The Quick Settings Tile is now automatically added to the tile panel when the module is enabled.

--- app/src/main/java/com/keshav/capturesposed/CaptureSposed.kt
+++ app/src/main/java/com/keshav/capturesposed/CaptureSposed.kt
@@ -1,10 +1,11 @@
 package com.keshav.capturesposed
 
-import android.annotation.SuppressLint
+import com.keshav.capturesposed.hookers.SystemUIHooker
 import com.keshav.capturesposed.hookers.WindowManagerServiceHooker
 import io.github.libxposed.api.XposedInterface
 import io.github.libxposed.api.XposedModule
 import io.github.libxposed.api.XposedModuleInterface.ModuleLoadedParam
+import io.github.libxposed.api.XposedModuleInterface.PackageLoadedParam
 import io.github.libxposed.api.XposedModuleInterface.SystemServerLoadedParam
 
 private lateinit var module: CaptureSposed
@@ -14,7 +15,6 @@ class CaptureSposed(base: XposedInterface, param: ModuleLoadedParam) : XposedMod
         module = this
     }
 
-    @SuppressLint("PrivateApi")
     override fun onSystemServerLoaded(param: SystemServerLoadedParam) {
         super.onSystemServerLoaded(param)
 
@@ -22,6 +22,24 @@ class CaptureSposed(base: XposedInterface, param: ModuleLoadedParam) : XposedMod
             WindowManagerServiceHooker.hook(param, module)
         } catch (e: Exception) {
             log("[CaptureSposed] ERROR: $e")
+        }
+    }
+
+    override fun onPackageLoaded(param: PackageLoadedParam) {
+        super.onPackageLoaded(param)
+
+        when (param.packageName) {
+            "com.android.systemui" -> {
+                val prefs = getRemotePreferences(BuildConfig.APPLICATION_ID)
+                if (!prefs.getBoolean("tileRevealDone", false)) {
+                    try {
+                        module.log("[CaptureSposed] Hooking System UI to add and reveal quick settings tile.")
+                        SystemUIHooker.hook(param, module)
+                    } catch (e: Exception) {
+                        log("[CaptureSposed] ERROR: $e")
+                    }
+                }
+            }
         }
     }
 }
---
+++ app/src/main/java/com/keshav/capturesposed/hookers/SystemUIHooker.kt
@@ -0,0 +1,60 @@
+package com.keshav.capturesposed.hookers
+
+import android.annotation.SuppressLint
+import com.keshav.capturesposed.BuildConfig
+import com.keshav.capturesposed.utils.XposedHelpers
+import io.github.libxposed.api.XposedInterface.BeforeHookCallback
+import io.github.libxposed.api.XposedInterface.Hooker
+import io.github.libxposed.api.XposedModule
+import io.github.libxposed.api.XposedModuleInterface.PackageLoadedParam
+import io.github.libxposed.api.annotations.BeforeInvocation
+import io.github.libxposed.api.annotations.XposedHooker
+
+object SystemUIHooker {
+    var module: XposedModule? = null
+    private const val tileId = "custom(${BuildConfig.APPLICATION_ID}/.QuickTile)"
+    private var tileRevealed = false
+
+    @SuppressLint("PrivateApi")
+    fun hook(param: PackageLoadedParam, module: XposedModule) {
+        this.module = module
+
+        module.hook(
+            param.classLoader.loadClass("com.android.systemui.qs.QSPanelControllerBase")
+                .getDeclaredMethod("setTiles"),
+            TileSetterHooker::class.java
+        )
+
+        /*module.hook(
+            param.classLoader.loadClass("com.android.systemui.qs.QSTileRevealController\$1")
+                .getDeclaredMethod("run"),
+            TileRevealAnimHooker::class.java
+        )*/
+    }
+
+    @XposedHooker
+    private class TileSetterHooker : Hooker {
+        companion object {
+            @JvmStatic
+            @BeforeInvocation
+            fun beforeInvocation(callback: BeforeHookCallback) {
+                if (!tileRevealed) {
+                    val tileHost = XposedHelpers.getObjectField(callback.thisObject, "mHost") as Any
+                    val tileHostClass = tileHost.javaClass as Class<*>
+
+                    // Depending on the Android distribution, the ordering of parameters is different.
+                    try {
+                        tileHostClass.getDeclaredMethod("addTile", Int::class.java, String::class.java)
+                            .invoke(tileHost, -1, tileId)
+                    }
+                    catch (t: Throwable) {
+                        tileHostClass.getDeclaredMethod("addTile", String::class.java, Int::class.java)
+                            .invoke(tileHost, tileId, -1)
+                    }
+
+                    module?.log("[MACsposed] Tile added to quick settings panel.")
+                }
+            }
+        }
+    }
+}
