5b0bd2dad6a16323abf8e1622f26fc8dfd01dc39
David Berdik <berdikd@duq.edu>
2024-05-18 16:47:05 -0400

    Use semantic versioning.

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -13,7 +13,7 @@ android {
         minSdk = 34
         targetSdk = 34
         versionCode = 1
-        versionName = "1.0"
+        versionName = "1.0.0"
 
         vectorDrawables {
             useSupportLibrary = true
