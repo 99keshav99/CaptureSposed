aa2f9c8910b7103ed0fb6bfbee7ef3497137ffbd
Tomasz K. <tomasz.k@e.email>
2024-05-21 19:08:11 +0200

    Introduce Material You dynamic colors
    (cherry picked from commit c7154ccd6a97a47c258368dfd89697163341017a)

--- app/src/main/java/com/keshav/capturesposed/ui/theme/Theme.kt
+++ app/src/main/java/com/keshav/capturesposed/ui/theme/Theme.kt
@@ -1,10 +1,14 @@
 package com.keshav.capturesposed.ui.theme
 
+import android.os.Build
 import androidx.compose.foundation.isSystemInDarkTheme
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.darkColorScheme
+import androidx.compose.material3.dynamicDarkColorScheme
+import androidx.compose.material3.dynamicLightColorScheme
 import androidx.compose.material3.lightColorScheme
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.platform.LocalContext
 
 private val DarkColorPalette = darkColorScheme(
     primary = Purple80,
@@ -20,9 +24,21 @@ private val LightColorPalette = lightColorScheme(
 
 @Composable
 fun APPTheme(
-    darkTheme: Boolean = isSystemInDarkTheme(), content: @Composable () -> Unit
+    darkTheme: Boolean = isSystemInDarkTheme(),
+    dynamicColor: Boolean = true,
+    content: @Composable () -> Unit
 ) {
+    val colorScheme = when {
+        dynamicColor && Build.VERSION.SDK_INT >= Build.VERSION_CODES.S -> {
+            val context = LocalContext.current
+            if (darkTheme) dynamicDarkColorScheme(context) else dynamicLightColorScheme(context)
+        }
+        darkTheme -> DarkColorPalette
+        else -> LightColorPalette
+    }
+
     MaterialTheme(
-        colorScheme = if (darkTheme) DarkColorPalette else LightColorPalette, content = content
+        colorScheme = colorScheme,
+        content = content
     )
 }
