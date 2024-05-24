aaa22b74ff705d11889de3633bbe1327c649fc74
David Berdik <berdikd@duq.edu>
2024-05-24 16:03:19 -0400

    Changed version code from 2 to 3 and version name from 1.0.1 to 1.0.2.

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -12,8 +12,8 @@ android {
         applicationId = "com.keshav.capturesposed"
         minSdk = 34
         targetSdk = 34
-        versionCode = 2
-        versionName = "1.0.1"
+        versionCode = 3
+        versionName = "1.0.2"
 
         vectorDrawables {
             useSupportLibrary = true
