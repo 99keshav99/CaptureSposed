d6bd26bd0e7f83e3e050fa621e7f2d0e4654c6d9
Tomasz K. <tomasz.k@e.email>
2024-05-24 11:16:53 +0200

    Redesign app layout

--- app/src/main/java/com/keshav/capturesposed/MainActivity.kt
+++ app/src/main/java/com/keshav/capturesposed/MainActivity.kt
@@ -8,21 +8,35 @@ import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.layout.wrapContentSize
+import androidx.compose.foundation.rememberScrollState
+import androidx.compose.foundation.verticalScroll
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Icon
+import androidx.compose.material3.LargeTopAppBar
 import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.OutlinedCard
+import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Switch
 import androidx.compose.material3.Text
+import androidx.compose.material3.TopAppBarDefaults
+import androidx.compose.material3.rememberTopAppBarState
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.MutableState
 import androidx.compose.runtime.mutableIntStateOf
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.input.nestedscroll.nestedScroll
+import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
+import androidx.core.content.ContextCompat.getString
 import com.keshav.capturesposed.ui.theme.APPTheme
 import com.keshav.capturesposed.utils.PrefsUtils
 import com.keshav.capturesposed.utils.XposedChecker
@@ -47,7 +61,7 @@ class MainActivity : ComponentActivity() {
         setContent {
             APPTheme {
                 Surface(
-                    modifier = Modifier.fillMaxSize(), color = MaterialTheme.colorScheme.background
+                    color = MaterialTheme.colorScheme.surfaceContainer
                 ) {
                     AppUI(counter.intValue)
                 }
@@ -74,56 +88,119 @@ class MainActivity : ComponentActivity() {
         unregisterScreenCaptureCallback(screenCaptureCallback)
     }
 
+    @OptIn(ExperimentalMaterial3Api::class)
     @Composable
     fun AppUI(counter: Int, modifier: Modifier = Modifier) {
-        Column(
+        val scrollBehavior = TopAppBarDefaults.exitUntilCollapsedScrollBehavior(rememberTopAppBarState())
+
+        Scaffold(
+            topBar = {
+                LargeTopAppBar(
+                    title = {
+                        Text(
+                            text = stringResource(R.string.xposed_name),
+                            color = MaterialTheme.colorScheme.onSurface
+                        )
+                    },
+                    scrollBehavior = scrollBehavior
+                )
+            },
             modifier = modifier
+                .nestedScroll(scrollBehavior.nestedScrollConnection)
                 .fillMaxSize()
-                .wrapContentSize(Alignment.Center),
-            verticalArrangement = Arrangement.Center,
-            horizontalAlignment = Alignment.CenterHorizontally
-        ) {
-            SwitchOrText(isSwitchOn)
-            Text(
-                text = "Counter: $counter",
-                fontSize = 40.sp,
-                textAlign = TextAlign.Center,
-                modifier = Modifier.padding(50.dp),
-                color = MaterialTheme.colorScheme.secondary
-            )
+        ) { p ->
+            Column(
+                modifier = modifier
+                    .fillMaxSize()
+                    .padding(p)
+                    .verticalScroll(rememberScrollState())
+                    .padding(20.dp),
+                verticalArrangement = Arrangement.spacedBy(20.dp),
+                horizontalAlignment = Alignment.Start
+            ) {
+                MainCard(isSwitchOn)
+                TestCard()
+            }
         }
     }
 
     @Composable
-    fun SwitchOrText(isSwitchOn: MutableState<Boolean>) {
-        if (XposedChecker.isEnabled()) {
-            Row(
-                verticalAlignment = Alignment.CenterVertically
-            ) {
-                Text(
-                    text = "CaptureSposed",
-                    fontSize = 40.sp,
-                    textAlign = TextAlign.Center,
-                    color = MaterialTheme.colorScheme.primary
-                )
+    fun MainCard(isSwitchOn: MutableState<Boolean>) {
+        OutlinedCard(modifier = Modifier.fillMaxWidth()){
+            Column(modifier = Modifier.padding(16.dp)){
+                Row(
+                    verticalAlignment = Alignment.CenterVertically,
+                    horizontalArrangement = Arrangement.spacedBy(10.dp)
+                ){
+                    if(XposedChecker.isEnabled() && isSwitchOn.value){
+                        Icon(painterResource(R.drawable.okay_24), getString(R.string.running))
+                    }else if (XposedChecker.isEnabled() && !isSwitchOn.value){
+                        Icon(painterResource(R.drawable.disabled_24), getString(R.string.stopped))
+                    }else{
+                        Icon(painterResource(R.drawable.error_24), getString(R.string.error))
+                    }
+                    Text(getString(R.string.status_title), fontSize = 24.sp)
+                }
+                if (XposedChecker.isEnabled()) {
+                    Row(
+                        verticalAlignment = Alignment.CenterVertically,
+                        horizontalArrangement = Arrangement.SpaceBetween,
+                        modifier = Modifier.fillMaxWidth()
+                    ){
+                        if(isSwitchOn.value){
+                            Text(
+                                text = getString(R.string.status_running),
+                                fontSize = 16.sp,
+                                textAlign = TextAlign.Center,
+                                color = MaterialTheme.colorScheme.onSurface
+                            )
+                        }else{
+                            Text(
+                                text = getString(R.string.status_stopped),
+                                fontSize = 16.sp,
+                                textAlign = TextAlign.Center,
+                                color = MaterialTheme.colorScheme.onSurface
+                            )
+                        }
 
-                Switch(
-                    checked = isSwitchOn.value,
-                    onCheckedChange = {
-                        PrefsUtils.toggleHookState()
-                    },
-                    modifier = Modifier.padding(10.dp)
+                        Switch(
+                            checked = isSwitchOn.value,
+                            onCheckedChange = { PrefsUtils.toggleHookState() },
+                            modifier = Modifier.padding(10.dp)
+                        )
+                    }
+                } else {
+                    Text(
+                        text = getString(LocalContext.current, R.string.module_disabled),
+                        fontSize = 20.sp,
+                        textAlign = TextAlign.Start,
+                        color = MaterialTheme.colorScheme.onSurface,
+                        modifier = Modifier.padding(10.dp)
+                    )
+                }
+            }
+        }
+    }
+
+    @Composable
+    fun TestCard(){
+        OutlinedCard(modifier = Modifier.fillMaxWidth()){
+            Column(modifier = Modifier.padding(16.dp)){
+                Row(
+                    verticalAlignment = Alignment.CenterVertically,
+                    horizontalArrangement = Arrangement.spacedBy(10.dp)
+                ){
+                    Icon(painterResource(R.drawable.checklist_24), getString(R.string.card_title_testing))
+                    Text(getString(R.string.card_title_testing), fontSize = 24.sp)
+                }
+                Text(
+                    text = "Counter: ${counter.intValue}",
+                    fontSize = 20.sp,
+                    textAlign = TextAlign.Start,
+                    modifier = Modifier.padding(10.dp),
+                    color = MaterialTheme.colorScheme.onSurface
                 )
             }
-        } else {
-            Text(
-                text = getString(R.string.module_disabled),
-                fontSize = 20.sp,
-                textAlign = TextAlign.Center,
-                color = MaterialTheme.colorScheme.tertiary,
-                modifier = Modifier.padding(10.dp)
-            )
         }
-
     }
 }
--- app/src/main/java/com/keshav/capturesposed/ui/theme/Theme.kt
+++ app/src/main/java/com/keshav/capturesposed/ui/theme/Theme.kt
@@ -1,5 +1,6 @@
 package com.keshav.capturesposed.ui.theme
 
+import android.annotation.SuppressLint
 import android.os.Build
 import androidx.compose.foundation.isSystemInDarkTheme
 import androidx.compose.material3.MaterialTheme
@@ -8,7 +9,16 @@ import androidx.compose.material3.dynamicDarkColorScheme
 import androidx.compose.material3.dynamicLightColorScheme
 import androidx.compose.material3.lightColorScheme
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.platform.LocalContext
+
+val Purple80 = Color(0xFFD0BCFF)
+val PurpleGrey80 = Color(0xFFCCC2DC)
+val Pink80 = Color(0xFFEFB8C8)
+
+val Purple40 = Color(0xFF6650a4)
+val PurpleGrey40 = Color(0xFF625b71)
+val Pink40 = Color(0xFF7D5260)
 
 private val DarkColorPalette = darkColorScheme(
     primary = Purple80,
@@ -22,6 +32,7 @@ private val LightColorPalette = lightColorScheme(
     tertiary = Pink40,
 )
 
+@SuppressLint("ObsoleteSdkInt")
 @Composable
 fun APPTheme(
     darkTheme: Boolean = isSystemInDarkTheme(),
