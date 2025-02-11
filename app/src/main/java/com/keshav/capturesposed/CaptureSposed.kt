cb153dffe3abf3597fd80c710634c58c6da4f2fc
David Berdik <berdikd@duq.edu>
2025-02-10 23:00:01 -0500

    Redesigned hook for blocking screen recording detection.

--- app/src/main/java/com/keshav/capturesposed/CaptureSposed.kt
+++ app/src/main/java/com/keshav/capturesposed/CaptureSposed.kt
@@ -1,7 +1,6 @@
 package com.keshav.capturesposed
 
 import android.os.Build
-import com.keshav.capturesposed.hookers.ScreenRecordingCallbackControllerHooker
 import com.keshav.capturesposed.hookers.SystemUIHooker
 import com.keshav.capturesposed.hookers.WindowManagerServiceHooker
 import io.github.libxposed.api.XposedInterface
@@ -22,9 +21,6 @@ class CaptureSposed(base: XposedInterface, param: ModuleLoadedParam) : XposedMod
 
         try {
             WindowManagerServiceHooker.hook(param, module)
-
-            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.VANILLA_ICE_CREAM)
-                ScreenRecordingCallbackControllerHooker.hook(param, module)
         } catch (e: Exception) {
             log("[CaptureSposed] ERROR: $e")
         }
--- app/src/main/java/com/keshav/capturesposed/hookers/WindowManagerServiceHooker.kt
+++ app/src/main/java/com/keshav/capturesposed/hookers/WindowManagerServiceHooker.kt
@@ -2,6 +2,7 @@ package com.keshav.capturesposed.hookers
 
 import android.annotation.SuppressLint
 import android.content.ComponentName
+import android.os.Build
 import com.keshav.capturesposed.BuildConfig
 import io.github.libxposed.api.XposedInterface.BeforeHookCallback
 import io.github.libxposed.api.XposedInterface.Hooker
@@ -12,16 +13,26 @@ import io.github.libxposed.api.annotations.XposedHooker
 
 object WindowManagerServiceHooker {
     var module: XposedModule? = null
+    var classLoader: ClassLoader? = null
 
     @SuppressLint("PrivateApi")
     fun hook(param: SystemServerLoadedParam, module: XposedModule) {
         this.module = module
+        classLoader = param.classLoader
+        val windowManagerServiceClass = param.classLoader.loadClass("com.android.server.wm.WindowManagerService")
 
         module.hook(
-            param.classLoader.loadClass("com.android.server.wm.WindowManagerService")
-                .getDeclaredMethod("notifyScreenshotListeners", Int::class.java),
+            windowManagerServiceClass.getDeclaredMethod("notifyScreenshotListeners", Int::class.java),
             NotifyScreenshotListenersHooker::class.java
         )
+
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.VANILLA_ICE_CREAM) {
+            module.hook(
+                windowManagerServiceClass.getDeclaredMethod("registerScreenRecordingCallback",
+                    classLoader!!.loadClass("android.window.IScreenRecordingCallback")),
+                RegisterScreenRecordingCallbackHooker::class.java
+            )
+        }
     }
 
     @XposedHooker
@@ -41,6 +52,21 @@ object WindowManagerServiceHooker {
                 else {
                     module?.log("[CaptureSposed] Allowed screenshot detection.")
                 }
+            }
+        }
+    }
+
+    @XposedHooker
+    private class RegisterScreenRecordingCallbackHooker: Hooker {
+        companion object {
+            @JvmStatic
+            @BeforeInvocation
+            fun beforeInvocation(callback: BeforeHookCallback) {
+                val prefs = module?.getRemotePreferences(BuildConfig.APPLICATION_ID)
+                val isHookActive = prefs?.getBoolean("screenRecordHookActive", true)
+
+                if (isHookActive!!)
+                    callback.returnAndSkip(false)
             }
         }
     }
