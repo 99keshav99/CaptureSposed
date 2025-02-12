b627c07826979a2f2445a2887d04d8bfe8eb3adc
David Berdik <berdikd@duq.edu>
2025-02-12 00:40:40 -0500

    Initial testing of using a custom wm command to force execution of a hooked method.

--- app/src/main/java/com/keshav/capturesposed/hookers/WindowManagerServiceHooker.kt
+++ app/src/main/java/com/keshav/capturesposed/hookers/WindowManagerServiceHooker.kt
@@ -3,6 +3,7 @@ package com.keshav.capturesposed.hookers
 import android.annotation.SuppressLint
 import android.content.ComponentName
 import android.os.Build
+import android.os.ResultReceiver
 import com.keshav.capturesposed.BuildConfig
 import io.github.libxposed.api.XposedInterface.BeforeHookCallback
 import io.github.libxposed.api.XposedInterface.Hooker
@@ -10,16 +11,22 @@ import io.github.libxposed.api.XposedModule
 import io.github.libxposed.api.XposedModuleInterface.SystemServerLoadedParam
 import io.github.libxposed.api.annotations.BeforeInvocation
 import io.github.libxposed.api.annotations.XposedHooker
+import java.io.FileDescriptor
+import java.lang.reflect.Method
 
 object WindowManagerServiceHooker {
     var module: XposedModule? = null
     var classLoader: ClassLoader? = null
+    lateinit var registerScreenRecordingCallbackMethod: Method
+    val screenRecordingCallbacks = mutableSetOf<Any>()
 
     @SuppressLint("PrivateApi")
     fun hook(param: SystemServerLoadedParam, module: XposedModule) {
         this.module = module
         classLoader = param.classLoader
         val windowManagerServiceClass = param.classLoader.loadClass("com.android.server.wm.WindowManagerService")
+        registerScreenRecordingCallbackMethod = windowManagerServiceClass.getDeclaredMethod("registerScreenRecordingCallback",
+            classLoader!!.loadClass("android.window.IScreenRecordingCallback"))
 
         module.hook(
             windowManagerServiceClass.getDeclaredMethod("notifyScreenshotListeners", Int::class.java),
@@ -27,10 +34,13 @@ object WindowManagerServiceHooker {
         )
 
         if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.VANILLA_ICE_CREAM) {
+            module.hook(registerScreenRecordingCallbackMethod, RegisterScreenRecordingCallbackHooker::class.java)
+
             module.hook(
-                windowManagerServiceClass.getDeclaredMethod("registerScreenRecordingCallback",
-                    classLoader!!.loadClass("android.window.IScreenRecordingCallback")),
-                RegisterScreenRecordingCallbackHooker::class.java
+                windowManagerServiceClass.getDeclaredMethod("onShellCommand", FileDescriptor::class.java,
+                    FileDescriptor::class.java, FileDescriptor::class.java, Array<String>::class.java,
+                    classLoader!!.loadClass("android.os.ShellCallback"), ResultReceiver::class.java),
+                OnShellCommandHooker::class.java
             )
         }
     }
@@ -62,11 +72,30 @@ object WindowManagerServiceHooker {
             @JvmStatic
             @BeforeInvocation
             fun beforeInvocation(callback: BeforeHookCallback) {
+                module?.log("[CaptureSposed] WE ARE INSIDE THE REGISTRATION")
+                screenRecordingCallbacks.add(callback.args[0])
                 val prefs = module?.getRemotePreferences(BuildConfig.APPLICATION_ID)
                 val isHookActive = prefs?.getBoolean("screenRecordHookActive", true)
 
                 if (isHookActive!!)
                     callback.returnAndSkip(false)
+            }
+        }
+    }
+
+    @XposedHooker
+    private class OnShellCommandHooker: Hooker {
+        companion object {
+            @JvmStatic
+            @BeforeInvocation
+            fun beforeInvocation(callback: BeforeHookCallback) {
+                // This will intercept command: wm refresh-recording-callbacks
+                val wmCommandArgs = callback.args[3] as Array<*>
+                if (wmCommandArgs.size == 1 && wmCommandArgs[0] == "refresh-recording-callbacks") {
+                    for (screenRecordingCallback in screenRecordingCallbacks) {
+                        registerScreenRecordingCallbackMethod.invoke(callback.thisObject, screenRecordingCallback)
+                    }
+                }
             }
         }
     }
